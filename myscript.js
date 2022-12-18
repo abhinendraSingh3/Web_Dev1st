10964f9a1983bd37925137ccbd883ea87a04da69
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-12-18 23:58:49 +0530

    .

--- myscript.js
+++ myscript.js
@@ -5,8 +5,36 @@ btntranslator.addEventListener("click",clickCall);
 var inArea=document.querySelector("#text-area");
 var outarea=document.querySelector("#out-area");
 
+// var apiurl='https://api.funtranslations.com/translate/minion.json';
+
+// function urltranslation(text){
+//   return apiurl+"?"+"text="+text
+// }
+
+
 function clickCall(){
     outarea.innerText=inArea.value;
+    console.log("clicked")
+
+// fetch(urltranslation)
+// .then((response)=>{
+//  return response.json();
+// })
+// .then((actualdata)=>{
+//     return console.log(actualdata.contents.translated)
+// })
+// .catch((error)=>{
+//     console.log(error);
+//     alert("something broke down");
+// })
 }
+fetch('https://api.funtranslations.com/translate/minion.json')
+.then((apidata)=>{
+    console.log(apidata)    
+})
+.then((response)=>{console.log(response.content)})
+
+
+
 
 
